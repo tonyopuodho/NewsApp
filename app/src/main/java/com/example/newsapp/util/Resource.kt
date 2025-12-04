a1ec61771ae6471ec333670e926753a1c257564e
TONY OCHIENG <tonyopuodho@gmail.com>
2025-12-04 14:40:02 +0300

    adding resource sealed class

---
+++ app/src/main/java/com/example/newsapp/util/Resource.kt
@@ -0,0 +1,6 @@
+package com.example.newsapp.util
+//sealed class used to show success or error during api call
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?): Resource<T>(data = data)
+    class Error<T>(message: String?): Resource<T>(message = message)
+}
