e75e8567fdbcb55d961ab0616ee42b4efc3346d0
TONY OCHIENG <tonyopuodho@gmail.com>
2025-12-05 08:54:46 +0300

    Adding top bar navigation

---
+++ app/src/main/java/com/example/newsapp/presentation/components/TopBar.kt
@@ -0,0 +1,33 @@
+package com.example.newsapp.presentation.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    onMoverTClick: () -> Unit
+){
+    TopAppBar(
+        title = { Text(text = "News Reader", fontSize = 20.sp, color = Color.White)},
+        actions = {
+            IconButton(onClick = onMoverTClick) {
+                Icon(imageVector = Icons.Default.MoreVert, contentDescription = "icon")
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Red,
+            titleContentColor = Color.White,
+            actionIconContentColor = Color.White
+        )
+    )
+}
--- app/src/main/java/com/example/newsapp/presentation/newsscreen/NewsScreen.kt
+++ app/src/main/java/com/example/newsapp/presentation/newsscreen/NewsScreen.kt
@@ -2,26 +2,42 @@ package com.example.newsapp.presentation.newsscreen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.newsapp.presentation.components.NewsArticleCard
+import com.example.newsapp.presentation.components.TopBar
 
 @Composable
 fun NewsScreen(
     viewModel: NewsViewModel = hiltViewModel()
 ){
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(3.dp),
-        contentPadding = PaddingValues(1.dp)
-    ) {
-        items(viewModel.articles){ article ->
-            NewsArticleCard(
-                article = article,
-                onClick = {}
-            )
+    /**
+     * using scaffold to place topbar navigation and bottom bar navigation
+     */
+
+    Scaffold(
+        topBar = {
+            TopBar(onMoverTClick = {})
         }
+    ) {innerPadding ->
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(3.dp),
+            contentPadding = PaddingValues(1.dp),
+            modifier = Modifier.padding(innerPadding).padding(top = 2.dp)
+        ) {
+            items(viewModel.articles){ article ->
+                NewsArticleCard(
+                    article = article,
+                    onClick = {}
+                )
+            }
+        }
+
     }
 }
