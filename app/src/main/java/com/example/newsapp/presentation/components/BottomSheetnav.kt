c0b7bc22d9149dace4ce8405b9d1f88da9d14428
TONY OCHIENG <tonyopuodho@gmail.com>
2025-12-06 09:52:47 +0300

    adding bottom navigation to show full read

---
+++ app/src/main/java/com/example/newsapp/presentation/components/BottomSheetnav.kt
@@ -0,0 +1,67 @@
+package com.example.newsapp.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.newsapp.domain.model.Article
+
+@Composable
+fun BottomSheetNav(
+    article: Article,
+    onReadFullStory: () -> Unit
+){
+    Surface(modifier = Modifier.padding(16.dp)) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = article.title,
+                style = MaterialTheme.typography.titleMedium,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = article.description ?: "",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            ImageHolder(imageUrl = article.urlToImage)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = article.content,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = article.author,
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Text(
+                    text = article.source.name ?: "",
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = onReadFullStory, modifier = Modifier.fillMaxWidth() ) {
+                Text(text = "Read full story")
+            }
+        }
+    }
+
+}
