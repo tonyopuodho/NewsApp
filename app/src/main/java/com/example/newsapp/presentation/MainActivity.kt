c5296a20f58ab534153a7ebfab28544cc8ab93d9
TONY OCHIENG <tonyopuodho@gmail.com>
2025-12-14 16:29:44 +0300

    implementing pull to refresh functionality

--- app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
@@ -5,12 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.newsapp.presentation.newsscreen.NewsScreen
 import com.example.newsapp.presentation.newsscreen.NewsViewModel
 import com.example.newsapp.util.NavigationController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -22,9 +40,31 @@ class MainActivity : ComponentActivity() {
             )
         )
         setContent {
-            val navController = rememberNavController()
-            NavigationController(navController = navController)
-
+            RefreshScreen()
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RefreshScreen(){
+    var isRefreshing by remember {
+        mutableStateOf(false)
+    }
+    val scope = rememberCoroutineScope()
+    PullToRefreshBox(
+        modifier = Modifier.fillMaxSize(),
+        state = rememberPullToRefreshState(),
+        isRefreshing = isRefreshing,
+        onRefresh = {
+            scope.launch {
+                isRefreshing = true
+                delay(2000)
+                isRefreshing = false
+            }
+        }
+    ) {
+        val navController = rememberNavController()
+        NavigationController(navController = navController)
     }
 }
